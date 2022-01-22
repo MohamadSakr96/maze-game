4278d429ae967b2eb6dc19c649bfcdc299ad0eca
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-22 15:56:32 +0200

    add: you loose function

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
-console.log("Hello");
+// load the page and only start when to buttong S is pressed
+window.onload = () => {
+    
+    // start the game?
+    youLoose();
+    
+
+}
+
+// Where the game starts
+function startGame() {
+
+
+
+}
+
+
+function youLoose() {
+    let elements = document.getElementsByClassName('boundary');
+    for (let i=0; i<elements.length; i++) {
+        elements[i].style.backgroundColor = "red";
+    }
+    document.getElementById('status').innerHTML = "You Loose 🙁";
+}
+
