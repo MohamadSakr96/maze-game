9a5098f7a2a21a81e830aefded0c786b41263469
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-22 16:14:35 +0200

    add: reset function

--- game.js
+++ game.js
@@ -1,25 +1,40 @@
+
+var state_of_game = false;
+
 // load the page and only start when to buttong S is pressed
 window.onload = () => {
-    
     // start the game?
-    youLoose();
+    
+    document.getElementById('start').addEventListener("click", startGame);
     
 
 }
 
 // Where the game starts
 function startGame() {
-
-
+    if (state_of_game == false) {
+        looseGame();
+    }else {
+        resetGame();
+    }
 
 }
 
 
-function youLoose() {
+function looseGame() {
     let elements = document.getElementsByClassName('boundary');
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "red";
     }
     document.getElementById('status').innerHTML = "You Loose 🙁";
+    state_of_game = true;
 }
 
+function resetGame() {
+    let elements = document.getElementsByClassName('boundary');
+    for (let i=0; i<elements.length; i++) {
+        elements[i].style.backgroundColor = "#eeeeee";
+    }
+    document.getElementById('status').innerHTML = "";
+    state_of_game = false;
+}
