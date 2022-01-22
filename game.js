051274c5b4fd487d58c2570dc34e1d65c7a2e681
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-22 20:46:01 +0200

    fix: cleaned my code

--- game.js
+++ game.js
@@ -1,23 +1,16 @@
 // make it into class later...
 var game_state = true;
 var game_score = 0;
-var count = 0;
+
 
 // load the page and only start when to buttong S is pressed
-window.onload = () => {
-    // start the game?
-    
+window.onload = () => {   
     document.getElementById('start').addEventListener("click", startGame);
-    
-    console.log(`OnLoad: game state is ${game_state} and counter is ${count}`);
-
 }
 
 // Where the game starts
 function startGame() {
     if (game_state == true) {
-        count += 1; 
-        console.log(`start game: game state is ${game_state} and counter is ${count}`);
         trackMouse();
     }else {
         resetGame();
@@ -29,43 +22,35 @@ function trackMouse() {
     let elements = document.getElementsByClassName('boundary');
 
     for (let i=0; i<elements.length; i++) {
-        if (game_state == true) {
-            elements[i].addEventListener("mouseover", looseGame);
-        }
+        elements[i].addEventListener("mouseover", looseGame);
     }
-    if (game_state == true) {
-        document.getElementById('end').addEventListener("mouseover", winGame);
-    }
+    document.getElementById('end').addEventListener("mouseover", winGame);
 }
 
 
-function winGame() {
-    if (game_state == true) {    
-        let elements = document.getElementsByClassName('boundary');
-        for (let i=0; i<elements.length; i++) {
-            elements[i].style.backgroundColor = "blue";
-        }
-        document.getElementById('status').innerHTML = "🎉 YOU WIN!! 🎉";
-        game_state = false;
-        game_score += 5;
-        disableEvent();
+function winGame() {      
+    let elements = document.getElementsByClassName('boundary');
+
+    for (let i=0; i<elements.length; i++) {
+        elements[i].style.backgroundColor = "blue";
     }
+    game_state = false;
+    game_score += 5;
+    document.getElementById('status').innerHTML += `<br><br> 🎉 YOU WIN!! 🎉 <br> ${game_score}`;
+    disableEvent();
 }
 
 
 function looseGame() {
-    console.log("hon?", game_state);
-    if (game_state == true) {
-        let elements = document.getElementsByClassName('boundary');
-        for (let i=0; i<elements.length; i++) {
-            elements[i].style.backgroundColor = "red";
-        }
-        document.getElementById('status').innerHTML = "🙁 YOU LOOSE!! 🙁";
-        game_state = false;
-        game_score -= 10;
-        disableEvent();
-    }
+    let elements = document.getElementsByClassName('boundary');
     
+    for (let i=0; i<elements.length; i++) {
+        elements[i].style.backgroundColor = "red";
+    }
+    game_state = false;
+    game_score -= 10;
+    document.getElementById('status').innerHTML += `<br><br> 🙁 YOU LOOSE!! 🙁 <br> ${game_score}`;
+    disableEvent();   
 }
 
 function resetGame() {
@@ -73,7 +58,7 @@ function resetGame() {
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "#eeeeee";
     }
-    document.getElementById('status').innerHTML = "";
+    document.getElementById('status').innerHTML = 'Begin by moving your mouse over the "S".';
     game_state = true;
     game_score = 0; // before reseting show final score ?
 }
