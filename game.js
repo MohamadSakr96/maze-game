5debeb11e5cd780fd3cad54a2081c38798d90d2e
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-22 16:31:12 +0200

    add: win function plus scoring system

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
-
-var state_of_game = false;
+// make it into class later...
+var game_state = false;
+var game_score = 0;
 
 // load the page and only start when to buttong S is pressed
 window.onload = () => {
@@ -13,11 +14,21 @@ window.onload = () => {
 // Where the game starts
 function startGame() {
     if (state_of_game == false) {
-        looseGame();
+        trackMouse();
     }else {
         resetGame();
     }
+}
 
+
+function winGame() {
+    let elements = document.getElementsByClassName('boundary');
+    for (let i=0; i<elements.length; i++) {
+        elements[i].style.backgroundColor = "blue";
+    }
+    document.getElementById('status').innerHTML = "🎉 YOU WIN!! 🎉";
+    game_state = true;
+    game_score += 5;
 }
 
 
@@ -26,8 +37,9 @@ function looseGame() {
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "red";
     }
-    document.getElementById('status').innerHTML = "You Loose 🙁";
-    state_of_game = true;
+    document.getElementById('status').innerHTML = "🙁 YOU LOOSE!! 🙁";
+    game_state = true;
+    game_score -= 10;
 }
 
 function resetGame() {
@@ -36,5 +48,6 @@ function resetGame() {
         elements[i].style.backgroundColor = "#eeeeee";
     }
     document.getElementById('status').innerHTML = "";
-    state_of_game = false;
+    game_state = false;
+    game_score = 0; // before reseting show final score ?
 }
