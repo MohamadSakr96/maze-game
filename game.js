6833d3353de5252337e2fe625abd0b05af4b94bb
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-24 13:29:33 +0200

    change: mouseenter instead of mouseover

--- game.js
+++ game.js
@@ -1,18 +1,18 @@
 // Game state variables
-var game_state = true;
+var game_state = true; // if true you can start the game, when false (then you are playing)
 var game_score = 0;
 
 // load the page and only start when to button S is pressed
 window.onload = () => {  
     if (game_state == true) {
-        document.getElementById('start').addEventListener("mouseover", trackMouse);
+        document.getElementById('start').addEventListener("mouseenter", trackMouse);
     }
 }
 
 // tracking if the mouse hit a boundary or reach the end
 function trackMouse() {
+    game_state = false;
     let elements = document.getElementsByClassName('boundary');
-
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "black";
         elements[i].addEventListener("mouseover", looseGame);
@@ -29,7 +29,7 @@ function winGame() {
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "green";
     }
-    game_state = false;
+    game_state = true;
     game_score += 5;
     document.getElementById('status').innerHTML = `🎉 YOU WIN!! 🎉 <br> ${game_score} points`;
     disableEvent();
@@ -42,7 +42,7 @@ function looseGame() {
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "red";
     }
-    game_state = false;
+    game_state = true;
     game_score -= 10;
     document.getElementById('status').innerHTML = `🙁 YOU LOOSE!! 🙁 <br> ${game_score} points`;
     disableEvent();   
@@ -56,7 +56,7 @@ function resetGame() {
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "#eeeeee";
     }
-    document.getElementById('status').innerHTML = `Begin by moving your mouse over the "S". <br> ${game_score} points`;
+    document.getElementById('status').innerHTML = `Begin by moving your mouse over the "S".`;
     disableEvent();
 }
 
