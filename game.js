44000272fd78bef67edd1e37353d51a612598a23
MohamadSakr96 <97542549+MohamadSakr96@users.noreply.github.com>
2022-01-24 12:46:01 +0200

    fix: start game on mouseover and only reset score on click

--- game.js
+++ game.js
@@ -2,20 +2,10 @@
 var game_state = true;
 var game_score = 0;
 
-
 // load the page and only start when to button S is pressed
 window.onload = () => {  
-    let a = 1; 
-    document.getElementById('start').addEventListener("click", () => {startGame();});
-}
-
-// Where the game starts
-function startGame() {
-
     if (game_state == true) {
-        trackMouse();
-    }else {
-        resetGame();
+        document.getElementById('start').addEventListener("mouseover", trackMouse);
     }
 }
 
@@ -27,18 +17,10 @@ function trackMouse() {
         elements[i].style.backgroundColor = "black";
         elements[i].addEventListener("mouseover", looseGame);
     }
+    document.getElementById('start').addEventListener("click", resetGame);
     document.getElementById('end').addEventListener("mouseover", winGame);
-    document.getElementById('game').addEventListener("mouseleave", outGame);
+    document.getElementById('game').addEventListener("mouseleave", looseGame);
 }
-
-// if you go outside of the game
-function outGame() {
-    if (game_state == true) {
-        alert("😁 Try Again! 😁");
-        looseGame();
-    }
-}
-
 
 // if you reach the end!!
 function winGame() {      
@@ -49,7 +31,7 @@ function winGame() {
     }
     game_state = false;
     game_score += 5;
-    document.getElementById('status').innerHTML += `<br><br> 🎉 YOU WIN!! 🎉 <br> ${game_score} points`;
+    document.getElementById('status').innerHTML = `🎉 YOU WIN!! 🎉 <br> ${game_score} points`;
     disableEvent();
 }
 
@@ -62,19 +44,20 @@ function looseGame() {
     }
     game_state = false;
     game_score -= 10;
-    document.getElementById('status').innerHTML += `<br><br> 🙁 YOU LOOSE!! 🙁 <br> ${game_score} points`;
+    document.getElementById('status').innerHTML = `🙁 YOU LOOSE!! 🙁 <br> ${game_score} points`;
     disableEvent();   
 }
 
 // only when you press "S" again
 function resetGame() {
     let elements = document.getElementsByClassName('boundary');
+    game_state = true;
+    game_score = 0; 
     for (let i=0; i<elements.length; i++) {
         elements[i].style.backgroundColor = "#eeeeee";
     }
-    document.getElementById('status').innerHTML = 'Begin by moving your mouse over the "S".';
-    game_state = true;
-    game_score = 0; // before reseting show final score ?
+    document.getElementById('status').innerHTML = `Begin by moving your mouse over the "S". <br> ${game_score} points`;
+    disableEvent();
 }
 
 // to stop event listeners from always running
@@ -84,6 +67,6 @@ function disableEvent() {
         elements[i].removeEventListener("mouseover", looseGame);
     }
     document.getElementById('end').removeEventListener("mouseover", winGame);
-    document.getElementById('game').removeEventListener("mouseleave", outGame);
+    document.getElementById('game').removeEventListener("mouseleave", looseGame);
 }
 
